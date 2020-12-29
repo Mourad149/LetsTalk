fd7e3f2a2389bcb5305ed5fa6d0e7c4b944b2957
zakaria azziz <zakaria_azziz@um5.ac.ma>
2020-12-29 17:40:31 +0100

    some styling

--- src/meetings-page/meetings-page.style.js
+++ src/meetings-page/meetings-page.style.js
@@ -1,22 +1,18 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   meetingsPage: {
-    height: '100%',
-    width: '100%',
-    background: '#304352' /* fallback for old browsers */,
-    background:
-      '-webkit-linear-gradient(to left, #ffffff, #d7d2cc)' /* Chrome 10-25, Safari 5.1-6 */,
-    background:
-      'linear-gradient(to left, #ffffff, #d7d2cc)' /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */,
+    height: "100%",
+    width: "100%",
+    backgroundColor: "white",
   },
   gridItemContainer: {
-    height: '100%',
-    display: 'flex',
-    padding: '10px',
+    height: "100%",
+    display: "flex",
+    padding: "10px",
   },
   dividerContainer: {
-    padding: '20px 0 20px 5px',
+    padding: "20px 0 20px 5px",
   },
 });
 
